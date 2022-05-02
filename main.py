a93554b19419505745065106cf86be45d2ad9ff3
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-05-02 10:21:27 -0400

    fixed buttons

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 # Author: ATN 5/17/22
 
-import turtle
+from tkinter import *
 
-CURSOR_SIZE = 20
-FONT_SIZE = 12
-FONT = ('Arial', FONT_SIZE, 'bold')
+main = Tk()
 
-window = turtle.Screen()
-window.setup(1024, 768)
+main.geometry("1024x768")
+main.configure(bg='white')
 
-button_1 = turtle.Turtle()
-button_1.hideturtle()
-button_1.shape('square')
-button_1.fillcolor('blue')
-button_1.penup()
-button_1.goto(-256, 0)
-button_1.write('Examen', align = 'center', font=FONT)
-button_1.sety(0 + CURSOR_SIZE + FONT_SIZE)
-# button_1.onclick(window.bgcolor('black'))
-button_1.showturtle()
+def close_window():
+    exit()
 
 
-window.mainloop()
+examen = Button(main, text='Examen')
+examen.pack()
+
+close = Button(main, text='Exit', command=close_window)
+close.pack()
+
+mainloop()
